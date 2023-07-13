f948f86fe0dedd4298b73b3f475c18b83e2e8d8c
Liam Reid <cl2reid@gmail.com>
2023-07-13 09:45:29 -0400

    configure tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
+  themes: [
+    {
+      custom: {
+        primary: "#31393f",
+        secondary: "#272b21",
+        accent: "#9b7d8f",
+        neutral: "#31393f",
+        "base-100": "#030302",
+        info: "#7cb9de",
+        success: "#86efac",
+        warning: "#fde047",
+        error: "#fb7185",
+      },
+    },
+  ],
   plugins: [require("daisyui")],
 };
